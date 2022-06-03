043a6e6df13f0f5261c757f8d2d654cfc2efd740
kinza <bsef19a515@pucit.edu.pk>
2022-06-03 20:17:56 +0500

    mainactivity2 changes

--- app/src/main/java/com/example/myapplication12/MainActivity2.java
+++ app/src/main/java/com/example/myapplication12/MainActivity2.java
@@ -15,10 +15,10 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         ArrayList<Book> bookArrayList = new ArrayList<>();
-        bookArrayList.add(new Book("Reclaim ", "Alpha", R.drawable.a1));
-        bookArrayList.add(new Book("We pray", "Beta",  R.drawable.a2));
-        bookArrayList.add(new Book("Prayers pious", "Alpha",  R.drawable.a3));
-        bookArrayList.add(new Book("The Right Duties", "Beta", R.drawable.a4));
+        bookArrayList.add(new Book("Reclaim Heart", "Yasmin Mogahed", R.drawable.a1));
+        bookArrayList.add(new Book("We pray", "Ameenah",  R.drawable.a2));
+        bookArrayList.add(new Book("Prayers pious", "Omer Suleiman",  R.drawable.a3));
+        bookArrayList.add(new Book("The Right Duties", "Abdul Ghaffar", R.drawable.a4));
         MyViewAdapter adapter = new MyViewAdapter(this, bookArrayList);
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(adapter);
